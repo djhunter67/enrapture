f3284cf81909f2292c37240bee16085427b6942c
Christerpher Hunter <djhunter67@gmail.com>
2025-10-04 18:18:02 -0500

    feat: create a user in the database

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use enrapture::hello;
+use enrapture::{
+    hello,
+    models::sqlite::{DATABASE_NAME, create_user},
+};
 
 fn main() {
     println!("{}", hello());
 
     // the sqlite schema is at src/models/schema.sql
     // use rusqlite to instantiate the database in memory
-    let conn = rusqlite::Connection::open("enrapture.db").expect("Failed to open database");
+    let conn = rusqlite::Connection::open(DATABASE_NAME).expect("Failed to open database");
     conn.execute_batch(include_str!("models/schema.sql"))
         .expect("Failed to create schema");
+    // close the connection
+    drop(conn);
 
     println!("Database initialized in memory.");
+
+    for _ in 0..5 {
+        let random_email_number = rand::random::<u8>();
+
+        let email = format!("user_{random_email_number}");
+
+        // Create an arbitrary user
+        create_user(&format!("{email}@email.com"), "password123");
+    }
 }
 
 #[cfg(test)]
--- src/models/sqlite.rs
+++ src/models/sqlite.rs
@@ -1,5 +1,7 @@
 use crate::security::hashing::hash_pw;
 
+pub const DATABASE_NAME: &str = "enrapture.db";
+
 /// Create a new user with the given email and clear password.
 /// The password is hashed using argon2 before being stored in the database.
 /// # Arguments
@@ -12,13 +14,14 @@ use crate::security::hashing::hash_pw;
 pub fn create_user(email: &str, clear_password: &str) {
     // hash the password using argon2
     let hashed_password = hash_pw(clear_password);
-    let conn = rusqlite::Connection::open("users.db").expect("Error opening database");
+    let conn = rusqlite::Connection::open(DATABASE_NAME).expect("Error opening database");
 
     conn.execute(
-        "INSERT INTO users (email, password) VALUES (?1, ?2, ?3)",
+        "INSERT INTO users (email, password_hash) VALUES (?1, ?2)",
         rusqlite::params![email, hashed_password],
     )
     .expect("Error inserting user");
+    drop(conn);
 
-    println!("{email} created successfully");
+    println!("\n{email} created successfully\n");
 }
